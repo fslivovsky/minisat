e6e009e436c995dbaba41a0de0a12c423282d5fe
Arie Gurfinkel <arie@cmu.edu>
2014-02-22 00:11:32 -0500

    factored TraceProofVisitor into separate files

--- core/ProofVisitor.h
+++ core/ProofVisitor.h
@@ -11,13 +11,12 @@
 #define PROOF_VISITOR_H_
 
 #include "SolverTypes.h"
-#include "Solver.h"
 
 namespace Minisat {
 
-class ProofVisitor
-{
-public:
+  class ProofVisitor
+  {
+  public:
     ProofVisitor() {}
 
     virtual int visitResolvent      (Lit parent, Lit p1, CRef p2) { return 0; }
@@ -26,29 +25,7 @@ public:
 
     vec<Lit>        chainPivots;
     vec<CRef>       chainClauses;
-};
-
- class TraceProofVisitor : public ProofVisitor
- {
- protected:
-   Solver &m_Solver;
-   CMap<int> m_visited;
-
-   vec<int> m_units;
-   int m_ids;
-   
-   void doAntecendents ();
-   
- public:
-   TraceProofVisitor (Solver &solver) : m_Solver (solver), m_ids(1) 
-   {
-     m_units.growTo (m_Solver.nVars (), -1);
-   }
-   
-   int visitResolvent (Lit parent, Lit p1, CRef p2);
-   int visitChainResolvent (Lit parent);
-   int visitChainResolvent (CRef parent);
- };
+  };
 }
 
-#endif /* G_PROOF_VISITOR_H_ */
+#endif /* PROOF_VISITOR_H_ */
--- core/TraceProofVisitor.cc
+++ core/TraceProofVisitor.cc
@@ -1,54 +1,58 @@
-#include "ProofVisitor.h"
-#include "SolverTypes.h"
-
+#include "TraceProofVisitor.h"
 
 namespace Minisat
 {
   namespace
   {
-    void toDimacs (Lit lit)
+    void toDimacs (FILE* out, Lit lit)
     {
-      printf ("%s%d", sign (lit) ? "-" : "", var (lit) + 1);
+      fprintf (out, "%s%d", sign (lit) ? "-" : "", var (lit) + 1);
     }
     
-    void toDimacs (const Clause &c)
+    void toDimacs (FILE* out, const Clause &c)
     {
       for (int i = 0; i < c.size (); ++i)
       {
-        toDimacs (c[i]);
-        printf (" ");
+        toDimacs (out, c[i]);
+        fprintf (out, " ");
       }
     }
     
   }
-  
+    
+  TraceProofVisitor::TraceProofVisitor (Solver &solver, FILE* out) 
+    : m_Solver (solver), m_ids(1), m_out (out) 
+   {
+     m_units.growTo (m_Solver.nVars (), -1);
+   }
+ 
   int TraceProofVisitor::visitResolvent (Lit parent, Lit p1, CRef p2)
   {
     if (m_units [var (p1)] < 0)
     {
       m_units [var (p1)] = m_ids++;
-      printf ("%d ", m_ids - 1);
-      toDimacs (p1);
-      printf (" 0 0\n");
+      fprintf (m_out, "%d ", m_ids - 1);
+      toDimacs (m_out, p1);
+      fprintf (m_out, " 0 0\n");
     }
     int id;
     if (!m_visited.has (p2, id))
     {
       m_visited.insert (p2, m_ids++);
-      printf ("%d ", m_ids - 1);
-      toDimacs (m_Solver.getClause (p2));
-      printf (" 0 0\n");
+      fprintf (m_out, "%d ", m_ids - 1);
+      toDimacs (m_out, m_Solver.getClause (p2));
+      fprintf (m_out, " 0 0\n");
     }
     
     m_units [var (parent)] = m_ids++;
     
-    printf ("%d ", m_ids - 1);
-    toDimacs (parent);
-    printf (" 0 ");
+    fprintf (m_out, "%d ", m_ids - 1);
+    toDimacs (m_out, parent);
+    fprintf (m_out, " 0 ");
     
     id = -1;
     m_visited.has (p2, id);
-    printf ("%d %d 0\n", m_units [var (p1)], id);
+    fprintf (m_out, "%d %d 0\n", m_units [var (p1)], id);
     
     return 0;
   }
@@ -59,24 +63,24 @@ namespace Minisat
     Var vp = var (parent);
     
     m_units [vp] = m_ids++;
-    printf ("%d ", m_ids-1);
-    toDimacs (parent);
-    printf (" 0 ");
+    fprintf (m_out, "%d ", m_ids-1);
+    toDimacs (m_out, parent);
+    fprintf (m_out, " 0 ");
 
     int id;
     m_visited.has (chainClauses [0], id);
-    printf ("%d ", id);
+    fprintf (m_out, "%d ", id);
     for (int i = 0; i < chainPivots.size (); ++i)
     {
       if (i+1 < chainClauses.size ())
       {
         m_visited.has (chainClauses [i+1], id);
-        printf ("%d ", id);
+        fprintf (m_out, "%d ", id);
       }
       else
-        printf ("%d ", m_units [var (chainPivots [i])]);
+        fprintf (m_out, "%d ", m_units [var (chainPivots [i])]);
     }
-    printf (" 0\n");
+    fprintf (m_out, " 0\n");
 
     return 0;
   }
@@ -87,9 +91,9 @@ namespace Minisat
     if (!m_visited.has (chainClauses [0], id))
     {
       m_visited.insert (chainClauses [0], m_ids++);
-      printf ("%d ", m_ids-1);
-      toDimacs (m_Solver.getClause (chainClauses [0]));
-      printf (" 0 0\n");
+      fprintf (m_out, "%d ", m_ids-1);
+      toDimacs (m_out, m_Solver.getClause (chainClauses [0]));
+      fprintf (m_out, " 0 0\n");
     }
     
     for (int i = 0; i < chainPivots.size (); ++i)
@@ -99,9 +103,9 @@ namespace Minisat
         if (!m_visited.has (chainClauses [i+1], id))
         {
           m_visited.insert (chainClauses [i+1], m_ids++);
-          printf ("%d ", m_ids-1);
-          toDimacs (m_Solver.getClause (chainClauses [i+1]));
-          printf (" 0 0\n");
+          fprintf (m_out, "%d ", m_ids-1);
+          toDimacs (m_out, m_Solver.getClause (chainClauses [i+1]));
+          fprintf (m_out, " 0 0\n");
         }
       }
       else
@@ -110,9 +114,9 @@ namespace Minisat
         if (m_units [vp] < 0)
         {
           m_units [vp] = m_ids++;
-          printf ("%d ", m_ids-1);
-          toDimacs (chainPivots [i]);
-          printf (" 0 0\n");
+          fprintf (m_out, "%d ", m_ids-1);
+          toDimacs (m_out, chainPivots [i]);
+          fprintf (m_out, " 0 0\n");
         }
       }
     }
@@ -126,24 +130,24 @@ namespace Minisat
       m_visited.insert (parent, m_ids++);
     else m_ids++;
     
-    printf ("%d ", m_ids-1);
-    if (parent != CRef_Undef) toDimacs (m_Solver.getClause (parent));
-    printf (" 0 ");
+    fprintf (m_out, "%d ", m_ids-1);
+    if (parent != CRef_Undef) toDimacs (m_out, m_Solver.getClause (parent));
+    fprintf (m_out, " 0 ");
     
     int id;
     m_visited.has (chainClauses [0], id);
-    printf ("%d ", id);
+    fprintf (m_out, "%d ", id);
     for (int i = 0; i < chainPivots.size (); ++i)
     {
       if (i+1 < chainClauses.size ())
       {
         m_visited.has (chainClauses [i+1], id);
-        printf ("%d ", id);
+        fprintf (m_out, "%d ", id);
       }
       else
-        printf ("%d ", m_units [var (chainPivots [i])]);
+        fprintf (m_out, "%d ", m_units [var (chainPivots [i])]);
     }
-    printf (" 0\n");
+    fprintf (m_out, " 0\n");
     return 0;
   }
 }
