1fe9cdeb682e0ecd3a8952d4f5bedc67c847cf69
Yakir Vizel <yvizel@cs.technion.ac.il>
2014-05-31 17:49:22 +0300

    Adjusting TraceProofVisitor

--- core/TraceProofVisitor.cc
+++ core/TraceProofVisitor.cc
@@ -72,7 +72,7 @@ namespace Minisat
     fprintf (m_out, "%d ", id);
     for (int i = 0; i < chainPivots.size (); ++i)
     {
-      if (i+1 < chainClauses.size ())
+      if (i+1 < chainClauses.size () && chainClauses [i+1] != CRef_Undef)
       {
         m_visited.has (chainClauses [i+1], id);
         fprintf (m_out, "%d ", id);
@@ -98,7 +98,7 @@ namespace Minisat
     
     for (int i = 0; i < chainPivots.size (); ++i)
     {
-      if (i + 1 < chainClauses.size ())
+      if (i + 1 < chainClauses.size () && chainClauses[i+1] != CRef_Undef)
       {
         if (!m_visited.has (chainClauses [i+1], id))
         {
@@ -139,7 +139,7 @@ namespace Minisat
     fprintf (m_out, "%d ", id);
     for (int i = 0; i < chainPivots.size (); ++i)
     {
-      if (i+1 < chainClauses.size ())
+      if (i+1 < chainClauses.size () && chainClauses[i+1] != CRef_Undef)
       {
         m_visited.has (chainClauses [i+1], id);
         fprintf (m_out, "%d ", id);
